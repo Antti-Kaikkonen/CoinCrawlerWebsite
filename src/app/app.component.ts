2a3975bb522f4785e89a529ce7960e5d51b98441
Antti-Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-10-31 21:59:53 +0200

    Added country flags to pie chart tooltip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,7 +63,89 @@ export class AppComponent {
         legend: {
             display: false
         },
-        responsive: false
+        responsive: false,
+        tooltips: {
+          xPadding: 4,
+          yPadding: 4,
+          // Disable the on-canvas tooltip
+          enabled: false,
+  
+          custom: function(tooltipModel) {
+            console.log("tooltipModel", tooltipModel);
+            // Tooltip Element
+            var tooltipEl = document.getElementById('chartjs-tooltip');
+  
+            // Create element on first render
+            if (!tooltipEl) {
+              tooltipEl = document.createElement('div');
+              tooltipEl.id = 'chartjs-tooltip';
+              tooltipEl.innerHTML = "<table></table>";
+              document.body.appendChild(tooltipEl);
+            }
+  
+            // Hide if no tooltip
+            if (tooltipModel.opacity === 0) {
+              tooltipEl.style.opacity = "0";
+              return;
+            }
+  
+            // Set caret Position
+            tooltipEl.classList.remove('above', 'below', 'no-transform');
+            if (tooltipModel.yAlign) {
+              tooltipEl.classList.add(tooltipModel.yAlign);
+            } else {
+              tooltipEl.classList.add('no-transform');
+            }
+  
+            function getBody(bodyItem) {
+              return bodyItem.lines;
+            }
+  
+            // Set Text
+            if (tooltipModel.body) {
+              var titleLines = tooltipModel.title || [];
+              var bodyLines = tooltipModel.body.map(getBody);
+  
+              var innerHtml = '<thead>';
+  
+              titleLines.forEach(function(title) {
+                  innerHtml += '<tr><th>' + title + '</th></tr>';
+              });
+              innerHtml += '</thead><tbody>';
+  
+              bodyLines.forEach(function(body, i) {
+                  var colors = tooltipModel.labelColors[i];
+                  var style = 'background:' + colors.backgroundColor;
+                  style += '; border-color:' + colors.borderColor;
+                  style += '; border-width: 2px';
+                  var span = '<span style="' + style + '"></span>';
+                  let components = body[0].split(":");
+                  let country = components[0].trim();
+                  let value = components[1].trim();
+                  let countryFlag = '<span class="flag-icon flag-icon-'+countries.getAlpha2Code(country, 'en').toLowerCase()+'"></span> ';
+                  innerHtml += '<tr><td>' + span + countryFlag + country +": "+ value + '</td></tr>';
+              });
+              innerHtml += '</tbody>';
+  
+              var tableRoot = tooltipEl.querySelector('table');
+              tableRoot.innerHTML = innerHtml;
+            }
+  
+            // `this` will be the overall tooltip
+            var position = this._chart.canvas.getBoundingClientRect();
+  
+            // Display, position, and set styles for font
+            tooltipEl.style.opacity = "1";
+            tooltipEl.style.position = 'absolute';
+            tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+            tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+            tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+            tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+            tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+            tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+            tooltipEl.style.pointerEvents = 'none';
+          }
+        }
       }
     });
     console.log("Chart initialized");
@@ -77,7 +159,7 @@ export class AppComponent {
   ngOnInit() {
     //this.initChart();
     this.countrycodes.forEach(countryCode => {
-      let numberOfNodes = Math.floor(Math.random()*1000);
+      let numberOfNodes = Math.floor(Math.pow(1+Math.random(), 10)-1);
       let twoletter = countries.alpha3ToAlpha2(countryCode);
       this.country2count[countryCode] = {
         numberOfNodes: numberOfNodes,
