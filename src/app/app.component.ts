5eadf48469f1513c249b579f5b0bbfdef5b65b18
Antti-Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-10-31 07:50:25 +0200

    Added random country data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3/d3.min.js';
+import * as Datamap from 'datamaps/dist/datamaps.world.js';
+import * as countries from 'i18n-iso-countries';
+
+declare var require: any
+const english_countries = require('i18n-iso-countries/langs/en.json');
+
+countries.registerLocale(english_countries);
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,315 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CoinCrawlerWebsite';
+
+  datamap: Datamap;
+
+  country2count = {}
+  twoletter2count = [];
+  ngOnInit() {
+
+    this.countrycodes.forEach(countryCode => {
+      let numberOfNodes = Math.floor(Math.random()*1000);
+      let twoletter = countries.alpha3ToAlpha2(countryCode);
+      this.country2count[countryCode] = {
+        numberOfNodes: numberOfNodes
+      }
+      if (twoletter !== undefined) 
+        this.twoletter2count.push({
+          key: twoletter.toLowerCase(),
+          name: countries.getName(countryCode, "en"), 
+          value: numberOfNodes
+        });
+    });
+
+    let onlyValues = Object.keys(this.country2count).map(iso => this.country2count[iso].numberOfNodes);
+    var minValue = Math.min.apply(null, onlyValues),
+    maxValue = Math.max.apply(null, onlyValues);
+
+    let paletteScale = d3.scale.linear()
+            .domain([minValue, maxValue])
+            .range(["#EFEFFF","#02386F"]);
+
+    Object.keys(this.country2count).forEach(iso => {
+        this.country2count[iso].fillColor = paletteScale(this.country2count[iso].numberOfNodes);
+    });
+
+    let container = document.getElementById('mapContainer');
+    if (container === null || container === undefined) return;
+    this.datamap = new Datamap({
+        element: container,
+        projection: 'mercator', // big world map
+        responsive: false,
+        // countries don't listed in dataset will be painted with this color
+        fills: { defaultFill: '#F5F5F5' },
+        data: this.country2count,
+        geographyConfig: {
+            borderColor: '#DEDEDE',
+            highlightBorderWidth: 2,
+            // don't change color on mouse hover
+            highlightFillColor: function(geo) {
+                return geo['fillColor'] || '#F5F5F5';
+            },
+            // only change border
+            highlightBorderColor: '#B7B7B7',
+            // show desired information in tooltip
+            popupTemplate: function(geo, data) {
+                // don't show tooltip if country don't present in dataset
+                if (!data) { return ; }
+                // tooltip content
+                return ['<div class="hoverinfo">',
+                    '<strong>', geo.properties.name, '</strong>',
+                    '<br>Nodes: <strong>', data.numberOfNodes, '</strong>',
+                    '</div>'].join('');
+            }
+        }
+    });
+  }
+
+  countrycodes = ["ABW",
+  "AFG",
+  "AGO",
+  "AIA",
+  "ALA",
+  "ALB",
+  "AND",
+  "ANT",
+  "ARE",
+  "ARG",
+  "ARM",
+  "ASM",
+  "ATA",
+  "ATF",
+  "ATG",
+  "AUS",
+  "AUT",
+  "AZE",
+  "BDI",
+  "BEL",
+  "BEN",
+  "BFA",
+  "BGD",
+  "BGR",
+  "BHR",
+  "BHS",
+  "BIH",
+  "BLM",
+  "BLR",
+  "BLZ",
+  "BMU",
+  "BOL",
+  "BRA",
+  "BRB",
+  "BRN",
+  "BTN",
+  "BVT",
+  "BWA",
+  "CAF",
+  "CAN",
+  "CCK",
+  "CHE",
+  "CHL",
+  "CHN",
+  "CIV",
+  "CMR",
+  "COD",
+  "COG",
+  "COK",
+  "COL",
+  "COM",
+  "CPV",
+  "CRI",
+  "CUB",
+  "CXR",
+  "CYM",
+  "CYP",
+  "CZE",
+  "DEU",
+  "DJI",
+  "DMA",
+  "DNK",
+  "DOM",
+  "DZA",
+  "ECU",
+  "EGY",
+  "ERI",
+  "ESH",
+  "ESP",
+  "EST",
+  "ETH",
+  "FIN",
+  "FJI",
+  "FLK",
+  "FRA",
+  "FRO",
+  "FSM",
+  "GAB",
+  "GBR",
+  "GEO",
+  "GGY",
+  "GHA",
+  "GIB",
+  "GIN",
+  "GLP",
+  "GMB",
+  "GNB",
+  "GNQ",
+  "GRC",
+  "GRD",
+  "GRL",
+  "GTM",
+  "GUF",
+  "GUM",
+  "GUY",
+  "HKG",
+  "HMD",
+  "HND",
+  "HRV",
+  "HTI",
+  "HUN",
+  "IDN",
+  "IMN",
+  "IND",
+  "IOT",
+  "IRL",
+  "IRN",
+  "IRQ",
+  "ISL",
+  "ISR",
+  "ITA",
+  "JAM",
+  "JEY",
+  "JOR",
+  "JPN",
+  "KAZ",
+  "KEN",
+  "KGZ",
+  "KHM",
+  "KIR",
+  "KNA",
+  "KOR",
+  "KWT",
+  "LAO",
+  "LBN",
+  "LBR",
+  "LBY",
+  "LCA",
+  "LIE",
+  "LKA",
+  "LSO",
+  "LTU",
+  "LUX",
+  "LVA",
+  "MAC",
+  "MAF",
+  "MAR",
+  "MCO",
+  "MDA",
+  "MDG",
+  "MDV",
+  "MEX",
+  "MHL",
+  "MKD",
+  "MLI",
+  "MLT",
+  "MMR",
+  "MNE",
+  "MNG",
+  "MNP",
+  "MOZ",
+  "MRT",
+  "MSR",
+  "MTQ",
+  "MUS",
+  "MWI",
+  "MYS",
+  "MYT",
+  "NAM",
+  "NCL",
+  "NER",
+  "NFK",
+  "NGA",
+  "NIC",
+  "NIU",
+  "NLD",
+  "NOR",
+  "NPL",
+  "NRU",
+  "NZL",
+  "OMN",
+  "PAK",
+  "PAN",
+  "PCN",
+  "PER",
+  "PHL",
+  "PLW",
+  "PNG",
+  "POL",
+  "PRI",
+  "PRK",
+  "PRT",
+  "PRY",
+  "PSE",
+  "PYF",
+  "QAT",
+  "REU",
+  "ROU",
+  "RUS",
+  "RWA",
+  "SAU",
+  "SDN",
+  "SEN",
+  "SGP",
+  "SGS",
+  "SHN",
+  "SJM",
+  "SLB",
+  "SLE",
+  "SLV",
+  "SMR",
+  "SOM",
+  "SPM",
+  "SRB",
+  "STP",
+  "SUR",
+  "SVK",
+  "SVN",
+  "SWE",
+  "SWZ",
+  "SYC",
+  "SYR",
+  "TCA",
+  "TCD",
+  "TGO",
+  "THA",
+  "TJK",
+  "TKL",
+  "TKM",
+  "TLS",
+  "TON",
+  "TTO",
+  "TUN",
+  "TUR",
+  "TUV",
+  "TWN",
+  "TZA",
+  "UGA",
+  "UKR",
+  "UMI",
+  "URY",
+  "USA",
+  "UZB",
+  "VAT",
+  "VCT",
+  "VEN",
+  "VGB",
+  "VIR",
+  "VNM",
+  "VUT",
+  "WLF",
+  "WSM",
+  "YEM",
+  "ZAF",
+  "ZMB",
+  "ZWE"];
 }
